6c7ffadc6e01c56b542938a8083d5668e2ada811
atweek <ramille655@gmail.com>
2023-01-09 12:11:34 +0300

    added the ability to create vm

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,9 +1,11 @@
 pipeline {
     agent any
     parameters {
-        choice(name: 'location', choices: ['New York City, United States', 'Amsterdam, the Netherlands', 'San Francisco, United States', 'Singapore','London, United Kingdom', 'Frankfurt, Germany', 'Toronto, Canada', 'Bangalore, India', 'Sydney, Australia'], description: 'A slug indicating the region where the Droplet will be created')
+        choice(name: 'location', choices: ['nyc1','nyc3','ams3','sfo3','sgp1','lon1','fra1','tor1','blr1','syd1'], description: 'A slug indicating the region where the Droplet will be createdi')
 
 	choice(name: 'size', choices: ["1 CPU 1 GB","1 CPU 0.5 GB"], description: 'A slug indicating the size of the Droplet')
+
+	string(name: 'time', description: 'Через сколько удвлить машину?()')
     }
     stages {
         stage('auth') {
@@ -23,8 +25,46 @@ pipeline {
             steps {
                 script {
 			echo "create"
+		//NYC1   New York City, United States   nyc1
+		//NYC3   New York City, United States   nyc3
+		//AMS3   Amsterdam, the Netherlands   ams3
+		//SFO3   San Francisco, United States   sfo3
+		//SGP1   Singapore   sgp1
+		//LON1   London, United Kingdom   lon1
+		//FRA1   Frankfurt, Germany   fra1
+		//TOR1   Toronto, Canada   tor1
+		//BLR1   Bangalore, India   blr1
+		//SYD1   Sydney, Australia   syd1
+		// doctl compute droplet create --region sfo2 --image ubuntu-18-04-x64 --size s-1vcpu-1gb <DROPLET-NAME>
+			if (params.size == "1 CPU 1 GB")
+			{
+				size = "s-1vcpu-1gb"
+			}
+			else
+			{
+				size = "s-1vcpu-0.5gb"
+			}
+			sh """
+				doctl compute droplet create --region ${params.location} --image ubuntu-22-04-x64 --size ${size} jen-auto
+			"""
                  }//script
 	     }//steps
+        }//stage
+	 stage('wait') {
+            steps {
+	         script {
+			sleep time: "${params.time}", unit: 'MINUTES'	
+                 }//script
+             }//steps
+        }//stage
+        stage('remove') {
+            steps {
+	         script {
+			sh """
+			doctl compute droplet delete jen-auto -f
+			"""
+                 }//script
+             }//steps
         }//stage
     }//stages
 }//end
